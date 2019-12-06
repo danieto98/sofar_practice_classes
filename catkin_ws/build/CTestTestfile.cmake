6da6c88550bb99f072aa31f09b2e9c4ecd7d1f7e
Yusha <syedyusha@gmail.com>
2019-12-06 17:43:06 +0100

    Catkin Workspace developed during the SofAR practice classes 2019-2020

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /home/yusha/catkin_ws/src
+# Build directory: /home/yusha/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(mybot_ws/src/mybot_control)
+subdirs(mybot_ws/src/mybot_description)
+subdirs(mybot_ws/src/mybot_gazebo)
+subdirs(mybot_ws/src/mybot_navigation)
+subdirs(gazebo_ros_demos/rrbot_control)
+subdirs(gazebo_ros_demos/rrbot_description)
+subdirs(gazebo_ros_demos/rrbot_gazebo)
+subdirs(beginner_tutorials)
+subdirs(gazebo_ros_demos/gazebo_tutorials)
