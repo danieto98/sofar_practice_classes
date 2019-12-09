e9f847b0a28d122cd64a49798d5ecf2909c32013
Yusha <syedyusha@gmail.com>
2019-12-09 13:04:50 +0100

    Removed build and devel directories

--- catkin_ws/build/mybot_ws/src/mybot_navigation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yusha/catkin_ws/src/mybot_ws/src/mybot_navigation
-# Build directory: /home/yusha/catkin_ws/build/mybot_ws/src/mybot_navigation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
